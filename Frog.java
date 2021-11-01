0c3e2b222d7cc90c7c2ec7af55191acad4f5822e
ChloChlo <93141020+Chlooow@users.noreply.github.com>
2021-11-01 18:09:03 +0100

    Add files via upload

--- Frog.java
+++ Frog.java
@@ -48,32 +48,28 @@ public class Frog implements IFrog {
 	public void move(Direction key) {
 
 		if (key == Direction.left){
-			if(pos.absc > game.width == 0) {
-				pos.absc = new Case(pos.absc - 1, pos.ord);
+			if(pos.absc > 0) {
+				pos = new Case(pos.absc - 1, pos.ord);
 			}
 		}
 		if (key == Direction.right){
-			if(pos.absc < game.width){
-				pos.absc = new Case(pos.absc + 1, pos.ord);
-
+			if(pos.absc < game.width - 1){
+				pos = new Case(pos.absc + 1, pos.ord);
 			}
 		}
 
 		if (key == Direction.down){
-			if(pos.ord >){
-
+			if(pos.ord > 0){
+				pos = new Case(pos.absc, pos.ord - 1);
 			}
 		}
 
 		if(key == Direction.up) {
-			if (pos.ord) {
-
+			if (pos.ord < game.height - 1) {
+				pos = new Case(pos.absc, pos.ord + 1);
 			}
 		}
 	}
-
-
-
 }
 
 
