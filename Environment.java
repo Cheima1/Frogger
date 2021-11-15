8616fde597b11c836f69fcb612f186956e6de997
ChloChlo <93141020+Chlooow@users.noreply.github.com>
2021-11-15 17:20:44 +0100

    Update Environment.java

--- Environment.java
+++ Environment.java
@@ -1,12 +1,31 @@
 package environment;
 
 import java.util.ArrayList;
+import util.Case
 
 import gameCommons.Case;
 import gameCommons.Game;
 import gameCommons.IEnvironment;
 
 public class Environment implements IEnvironment {
+	
+	// attributs 
+	
+	private Game game;
+	private ArrayList<Lane> 
+		
+	// constructeurs
+		
+	// methodes
+	public boolean is WinningPosition(Case c) {
+		
+	}
+	
+	public boolean isSafe(Case c) {
+	}
+	
+	public void update() {
+	}
 		
 	//TODO
 
